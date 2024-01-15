ff9e70c736769908739ff193d11ac2126bafcc7b
Qijia Liu <liumeo@pku.edu.cn>
2024-01-14 22:13:28 -0500

    set_layout and set_candidates

---
+++ include/webview_candidate_window.hpp
@@ -0,0 +1,30 @@
+#ifndef WEBVIEW_CANDIDATE_WINDOW_H
+#define WEBVIEW_CANDIDATE_WINDOW_H
+
+#include "candidate_window.hpp"
+#include "webview.h"
+
+namespace candidate_window {
+class WebviewCandidateWindow : public CandidateWindow {
+  public:
+    WebviewCandidateWindow();
+    ~WebviewCandidateWindow();
+    void set_layout(layout_t layout) override;
+    void set_preedit_mode(bool enabled) override {}
+    void set_preedit(const std::vector<std::string> &text,
+                     const std::vector<format_t> format) override {}
+    void set_labels(const std::vector<std::string> &labels) override {}
+    void set_candidates(const std::vector<std::string> &candidates) override;
+    void set_highlight_callback(std::function<void(size_t index)>) override {}
+    void set_select_callback(std::function<void(size_t index)>) override {}
+    void set_style(const void *style) override{};
+    void show() override;
+    void hide() override;
+
+  private:
+    void set_transparent_background();
+    webview::webview w_;
+    std::thread thread_;
+};
+} // namespace candidate_window
+#endif
---
+++ preview/preview.mm
@@ -0,0 +1,22 @@
+#include "webview_candidate_window.hpp"
+#import <Cocoa/Cocoa.h>
+
+#include <chrono>
+#include <thread>
+
+int main(int argc, const char *argv[]) {
+    @autoreleasepool {
+        NSApplication *application = [NSApplication sharedApplication];
+
+        std::unique_ptr<candidate_window::CandidateWindow> candidateWindow =
+            std::make_unique<candidate_window::WebviewCandidateWindow>();
+        auto t = std::thread([&] {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            candidateWindow->set_layout(candidate_window::layout_t::vertical);
+            candidateWindow->set_candidates({"虚假的", "候选词"});
+            candidateWindow->show();
+        });
+        [application run];
+    }
+    return 0;
+}
