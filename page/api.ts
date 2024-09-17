9524201a213d7724b2df8b2c883a5f02b2526092
Qijia Liu <liumeo@pku.edu.cn>
2024-09-16 21:06:27 -0400

    f5m: fix drag regression f1de408

--- page/api.ts
+++ page/api.ts
@@ -1,3 +1,4 @@
+import { fcitx } from './distribution'
 import {
   panel,
   hoverables,
@@ -261,38 +262,6 @@ hoverables.addEventListener('wheel', e => {
   }
   fcitx._page((<WheelEvent>e).deltaY > 0)
 })
-
-const distribution = <string>process.env.FCITX_DISTRIBUTION ?? ''
-
-let fcitx: FCITX
-
-if (distribution === 'fcitx5-js') {
-  fcitx = window.fcitx
-} else {
-  // @ts-expect-error f5m binds C++ function to JS global function, but we want to call fcitx._select for both f5m and f5j.
-  fcitx = window
-  window.fcitx = fcitx
-
-  /*
-    Don't pollute page's style for f5j
-    background: transparent, draw panel as you wish
-    margin: default is 8px
-    overflow: no scrollbar
-    width, height: big enough, disregard window size
-  */
-  const style = document.createElement('style')
-  style.innerHTML =
-`body {
-  background: rgb(0 0 0 / 0%);
-  margin: 0;
-  overflow: hidden;
-  width: 1920px;
-  height: 1080px;
-}`
-  document.head.append(style)
-}
-
-fcitx.distribution = distribution
 
 // JavaScript APIs that webview_candidate_window.cpp calls
 fcitx.setCandidates = setCandidates
