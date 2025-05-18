5bf79221e8f4a5a924fe20c8c02b93c6b741fd3a
Qijia Liu <liumeo@pku.edu.cn>
2025-05-18 00:33:54 -0400

    grid alignment in scroll mode (#91)

--- tests/test-scroll.spec.ts
+++ tests/test-scroll.spec.ts
@@ -10,22 +10,97 @@ test('Passively expand', async ({ page }) => {
   expect(cppCalls.filter(call => JSON.stringify(call) === '{"highlight":0}').length, 'Highlight is set on expand').toEqual(1)
 })
 
-test('With less candidates, scroll bar shows in animation and hides when done, but should not cause layout shift.', async ({ page }) => {
+test('Grid alignment', async ({ page }) => {
   await init(page)
+  // 011122
+  // 34
+  // The sum of original width of the first 4 candidates is less than 392px,
+  // but the 4th candidate goes to the next row.
   await page.evaluate(() => {
     const style = document.createElement('style')
-    // 392 < 66 * 6 < 400, so with scroll bar in animation the first row can only have 5 candidates.
     style.innerHTML = `
-      .fcitx-candidate {
-        width: 66px;
+      .fcitx-candidate:nth-child(1) {
+        width: 50px;
+      }
+      .fcitx-candidate:nth-child(3) {
+        width: 150px;
+      }
+      .fcitx-candidate:nth-child(5) {
+        width: 100px;
+      }
+      .fcitx-candidate:nth-child(7) {
+        width: 50px;
+      }
+      .fcitx-candidate:nth-child(9) {
+        width: 50px;
       }
     `
     document.head.append(style)
   })
-  await scrollExpand(page, ['1', '2', '3', '4', '5', '6', '7'])
-  while ((await getBox(page.locator('.fcitx-hoverables'))).height !== 60);
+  const texts = ['1', '2', '3', '4', '5']
+  await scrollExpand(page, texts)
+  const boxes = await Promise.all(texts.map((_, i) => getBox(candidate(page, i))))
+  for (const box of boxes) {
+    expect(box.height).toEqual(30)
+  }
+
+  expect(boxes[0].width).toEqual(65)
+  expect(boxes[1].width).toEqual(195)
+  expect(boxes[2].width).toEqual(130)
+  expect(boxes[3].width).toEqual(65)
+  expect(boxes[4].width).toEqual(65)
+
+  expect(boxes[0].y).toEqual(boxes[1].y)
+  expect(boxes[1].y).toEqual(boxes[2].y)
+  expect(boxes[2].y + boxes[2].height).toEqual(boxes[3].y)
+  expect(boxes[3].y).toEqual(boxes[4].y)
+
+  expect(boxes[0].x + boxes[0].width).toEqual(boxes[1].x)
+  expect(boxes[1].x + boxes[1].width).toEqual(boxes[2].x)
+  expect(boxes[3].x).toEqual(boxes[0].x)
+  expect(boxes[4].x).toEqual(boxes[1].x)
+
+  const lastDividerBox = await getBox(page.locator('.fcitx-divider:last-child'))
+  expect(lastDividerBox.width, 'No gap in the end').toBeGreaterThanOrEqual(260)
+})
+
+test('Long candidate', async ({ page }) => {
+  await init(page)
+
+  await scrollExpand(page, ['长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长', '短', '短'])
+  const longBox = await getBox(candidate(page, 0))
+  const shortBox = await getBox(candidate(page, 2))
+  expect(longBox.width).toEqual(390)
+  expect(longBox.height).toBeGreaterThan(shortBox.height)
+  expect(shortBox.width).toEqual(65)
+})
+
+test('No enough space in current row', async ({ page }) => {
+  await init(page)
+
+  // 00011
+  // 22
+  await page.evaluate(() => {
+    const style = document.createElement('style')
+    style.innerHTML = `
+      .fcitx-candidate:nth-child(1) {
+        width: 180px;
+      }
+      .fcitx-candidate:nth-child(3) {
+        width: 120px;
+      }
+      .fcitx-candidate:nth-child(5) {
+        width: 120px;
+      }
+    `
+    document.head.append(style)
+  })
+  await scrollExpand(page, ['1', '2', '3'])
   const firstBox = await getBox(candidate(page, 0))
-  const sixthBox = await getBox(candidate(page, 5))
-  expect(firstBox.x).toEqual(sixthBox.x)
-  expect(firstBox.y).toBeLessThan(sixthBox.y)
+  const thirdBox = await getBox(candidate(page, 2))
+  expect(firstBox.x).toEqual(thirdBox.x)
+  expect(firstBox.y + firstBox.height).toEqual(thirdBox.y)
+
+  const lastDividerBoxInFirstRow = await getBox(page.locator('.fcitx-divider:nth-child(4)'))
+  expect(lastDividerBoxInFirstRow.width, 'No gap in the first row').toBeGreaterThan(65)
 })
