35eeb28f2ced59a2f0c63805b0ba78b151622c76
Qijia Liu <liumeo@pku.edu.cn>
2025-11-12 22:40:38 -0500

    fix style when preedit is extremely long (#115)
    * horizontal: fill the hole
    * scroll: wrap preedit

--- tests/test-header.spec.ts
+++ tests/test-header.spec.ts
@@ -7,13 +7,13 @@ test('Square when single character auxUp for macOS 15', async ({ page }) => {
   const pane = panel(page)
   const side = 32
 
-  await updateInputPanel(page, '', 'A', '')
+  await updateInputPanel(page, '', 'A')
   await expect(pane).toHaveText('A')
   let box = await getBox(pane)
   expect(box.width).toEqual(side)
   expect(box.height).toEqual(side)
 
-  await updateInputPanel(page, '', '拼', '')
+  await updateInputPanel(page, '', '拼')
   await expect(pane).toHaveText('拼')
   box = await getBox(pane)
   expect(box.width).toEqual(side)
@@ -24,9 +24,9 @@ test('No text shift when preedit grows', async ({ page }) => {
   await init(page)
   const preedit = panel(page).locator('.fcitx-preedit')
 
-  await updateInputPanel(page, 'c', '', '')
+  await updateInputPanel(page, 'c')
   const cBox = await getTextBox(preedit, 0)
-  await updateInputPanel(page, 'ce', '', '')
+  await updateInputPanel(page, 'ce')
   const ceBox = await getTextBox(preedit, 0)
   expect(ceBox).toEqual(cBox)
 })
