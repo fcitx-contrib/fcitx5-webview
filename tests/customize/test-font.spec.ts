eb3aa478976b2071b2024c57afea126d29524aa0
Qijia Liu <liumeo@pku.edu.cn>
2026-06-18 22:40:59 -0400

    fix test

--- tests/customize/test-font.spec.ts
+++ tests/customize/test-font.spec.ts
@@ -6,5 +6,6 @@ test('Set font family', async ({ page }) => {
   await init(page)
   await setStyle(page, { Font: { TextFontFamily: { 0: 'cursive', 1: 'Plangothic P1' } } })
   await setCandidates(page, [{ text: '字A' }], 0)
-  await expect(candidate(page, 0).locator('.fcitx-text')).toHaveCSS('font-family', 'cursive, "Plangothic P1"')
+  // WebKit 26.4 gives 'cursive, "Plangothic P1"', while 26.5 removes quote.
+  await expect(candidate(page, 0).locator('.fcitx-text')).toHaveCSS('font-family', 'cursive, Plangothic P1')
 })
